25e38ace9dcaef67c97b70dda3249f13230ae5a5
Monu Rajput <monurajput@example.com>
2025-10-17 09:37:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,28 @@ function App() {
 
   return (
     <>
-      <nav>
+
+
+              <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="Home.jsx">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="About">About</a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </nav>    
+      {/* <nav>
 
         <a href="Home.jsx"> Home</a>
         <br /><br />
@@ -16,7 +37,7 @@ function App() {
         <a href="About.jsx">About</a>
 
 
-      </nav>
+      </nav> */}
 
       <h1>Hello sir</h1>
     
